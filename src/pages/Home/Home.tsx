adeb85d5a28779044e76b4133ddc9d3555e9cf56
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-09-27 10:52:24 +0300

    carousel to be done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,14 @@ import { Fade } from "react-awesome-reveal";
 import style from "./Home.module.css";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selector";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 
 const Home = () => {
   const isLoggendIn = useSelector(selectIsLoggedIn);
@@ -10,11 +18,32 @@ const Home = () => {
   return (
     <div>
       {!isLoggendIn && (
-        <Fade delay={1000}>
-          <h1 className={style.header}>
-            Привіт, я Аліна Смєлянець, твій Психолог
-          </h1>
-        </Fade>
+        <>
+          <Fade delay={1000}>
+            <h1 className={style.header}>
+              Привіт, я Аліна Смєлянець, твій Психолог
+            </h1>
+          </Fade>
+          <Carousel className="w-full max-w-xs">
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">
+                          Куня 
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </>
       )}
       {isLoggendIn && (
         <Fade className={style.header} delay={1000}>
