2c7ad7b627692b54f388ca19c819bb556111b8d4
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-10-01 19:16:29 +0300

    burger in prgrs

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styles from "./BurgerMenu.module.css";
 import "./BurgerMenu.css";
+import { logoutThunk } from "@/redux/auth/operations";
 
 const BurgerMenu = () => {
   const showSettings = (e) => {
@@ -15,20 +16,18 @@ const BurgerMenu = () => {
   const isLoggendIn = useSelector(selectIsLoggedIn);
 
   return (
-    <Menu right width={"100%"}>
-      <ul>
-        
-        {!isLoggendIn && (
-          <li>
-            <NavLink to="/login">Вхід</NavLink>
-          </li>
-        )}
-        {!isLoggendIn && (
-          <li>
-            <NavLink to="/register">Реєстрація</NavLink>
-          </li>
-        )}
-      </ul>
+    <Menu right width={"100%"} isOpen>
+      {!isLoggendIn && <NavLink to="/login">Вхід</NavLink>}
+      {!isLoggendIn && <NavLink to="/register">Реєстрація</NavLink>}
+      {isLoggendIn && (
+        <>
+          <NavLink to="/about">Про мене</NavLink>
+
+          <NavLink to="/calendar">Календар</NavLink>
+
+          <button onClick={() => dispatch(logoutThunk())}>Вихід</button>
+        </>
+      )}
     </Menu>
   );
 };
