810564c6e0bfe54453755c124662690ae8819ef9
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-10-03 14:06:11 +0300

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import Loader from "./components/Loader/Loader";
 import './index.css' 
 import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
 import AppBar from "./components/AppBar/AppBar";
-import { AppDispatch } from '@/redux/store';
+import { AppDispatch } from './redux/store';
 
 
 function App() {
