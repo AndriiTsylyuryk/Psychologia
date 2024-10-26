7eec68d2c70ffcc45ecf42b45b28efc8ae1cc943
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-10-26 11:46:21 +0300

    back for google api

--- src/components/Calendar/CalendarComponent.tsx
+++ src/components/Calendar/CalendarComponent.tsx
@@ -2,17 +2,31 @@ import React from "react";
 import moment from "moment";
 import FullCalendar from "@fullcalendar/react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import interactionPlugin from "@fullcalendar/interaction"; 
 import timeGridPlugin from "@fullcalendar/timegrid";
-
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import "./CustomStyles.css";
+import { myAPI } from "@/config/API";
 
 
 const Calendar = () => {
+  const handleDateSelect = async (selectInfo) => {
+    const { start, end } = selectInfo;
+    const title = prompt("Введіть деталі вашої зустрічі:");
+
+    if (title) {
+      try { await myAPI.post("/api/create-event", {
+          body: JSON.stringify({ start, end, title }),
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
   return (
     <div>
       <FullCalendar
-        plugins={[timeGridPlugin]}
+        plugins={[timeGridPlugin, googleCalendarPlugin,interactionPlugin ]}
         initialView="timeGridDay"
         height={600}
         nowIndicator={true}
@@ -21,18 +35,10 @@ const Calendar = () => {
         allDayText="Час"
         slotMinTime={"08:00:00"}
         buttonText={{ today: "Сьогодні" }}
-        events={[
-          {
-            title: "event1",
-            start: "2024-10-10 10:00",
-            end: "2024-10-10 13:00",
-          },
-          {
-            title: "event2",
-            start: "2010-01-05",
-            end: "2010-01-07",
-          },
-        ]}
+        googleCalendarApiKey="AIzaSyBPEauVSK3tecB8Wn2mtIiLhxdHb-BghEU"
+        events={{googleCalendarId: 'navigator.tsylyuryk@gmail.com'}}
+        selectable={true}
+        select={handleDateSelect}
       />
     </div>
   );
