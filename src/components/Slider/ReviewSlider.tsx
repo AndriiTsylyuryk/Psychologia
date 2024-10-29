67996460084c2b625ccc0a9b0dbc38ea338bd4b5
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-10-29 12:52:21 +0200

    lang upd

--- src/components/Slider/ReviewSlider.tsx
+++ src/components/Slider/ReviewSlider.tsx
@@ -1,45 +1,49 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import style from "./ReviewSlider.module.css";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
-
+import { useTranslation } from "react-i18next";
 
 export default function ReviewSlider() {
+  const { t } = useTranslation();
+
+  // Відгуки з використанням `t` для перекладу
   const carouselTexts = [
     {
-      text: "'Ви супер, допомогли справитись з залежністю'",
-      name: "Анна",
+      text: t("reviews.0.text"),
+      name: t("reviews.0.name")
     },
-    { text: "'Дякую за поміч'", name: "Микола" },
-    { text: "'Це наша не остання зусріч'", name: "Андрій" },
+    {
+      text: t("reviews.1.text"),
+      name: t("reviews.1.name")
+    },
+    {
+      text: t("reviews.2.text"),
+      name: t("reviews.2.name")
+    }
   ];
 
   return (
-    <>
-      <Swiper
-        pagination={{
-          type: "fraction",
-        }}
-        autoplay={{ delay: 4000, disableOnInteraction: false }}
-        // navigation={true}
-        modules={[Pagination, Navigation, Autoplay]}
-        className={style.swiper}
-        speed={1000}
-      >
-        {carouselTexts.map((item) => {
-          return (
-            <SwiperSlide className={style.swiperslide}>
-              <div className={style.naming}>
-                <span>{item.name}</span>
-              </div>
-              <p>{item.text}</p>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </>
+    <Swiper
+      pagination={{
+        type: "fraction"
+      }}
+      autoplay={{ delay: 4000, disableOnInteraction: false }}
+      modules={[Pagination, Navigation, Autoplay]}
+      className={style.swiper}
+      speed={1000}
+    >
+      {carouselTexts.map((item, index) => (
+        <SwiperSlide className={style.swiperslide} key={index}>
+          <div className={style.naming}>
+            <span>{item.name}</span>
+          </div>
+          <p>{item.text}</p>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
