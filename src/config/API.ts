51a3f46333ced9aa27e6fcae627f7371d0919129
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-11-03 13:36:19 +0200

    Update API.ts

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const myAPI = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://psychologia-eight.vercel.app/",
 });
 
 export const setToken = (token) => {
