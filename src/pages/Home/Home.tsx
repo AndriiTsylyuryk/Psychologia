69e3cd837557b71e9d8d40664167fffd283eaf2a
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-11-06 09:38:18 +0200

    styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,30 +13,32 @@ const Home = () => {
     <div className={style.home}>
       {!isLoggedIn && (
         <>
-          <div className={style.heroContainer}>
+          {/* <div className={style.heroContainer}> */}
             <div>
-              <h1 className={style.h1}>{t('hello')}</h1>
-                <span className={style.span}>{t("i am your psychologist")}</span>
+              <h1 className={style.h1}>{t("hello")}</h1>
+              <span className={style.span}>{t("i am your psychologist")}</span>
             </div>
-              <div className={style.heroImg}></div>
-          </div>
-          <ReviewSlider />
-          <div className={style.media}>
-            <a
-              href="https://www.instagram.com/alina_smelyanets_?igsh=YmVlaG83Nm1yaHN2"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LiaInstagram className={style.icon} size={50} />
-            </a>
-            <a
-              href="https://wa.me/37253704388"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaWhatsapp className={style.icon} size={50} />
-            </a>
-          </div>
+            <div className={style.heroImg}></div>
+          {/* </div> */}
+          {/* <div className={style.reviews}> */}
+            <ReviewSlider />
+            <div className={style.media}>
+              <a
+                href="https://www.instagram.com/alina_smelyanets_?igsh=YmVlaG83Nm1yaHN2"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LiaInstagram className={style.icon} size={50} />
+              </a>
+              <a
+                href="https://wa.me/37253704388"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp className={style.icon} size={50} />
+              </a>
+            </div>
+          {/* </div> */}
         </>
       )}
     </div>
